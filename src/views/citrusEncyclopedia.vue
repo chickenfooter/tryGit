8681a3da6f197560708a26b5432e47f5a30dd074
unknown <2018831579@qq.com>
2022-05-31 23:31:39 +0800

    注册功能

--- src/views/citrusEncyclopedia.vue
+++ src/views/citrusEncyclopedia.vue
@@ -1,3 +1,22 @@
 <template>
-    <div>柑橘百科</div>
+    <div>
+      <el-card shadow="never" style="margin-bottom: 15px">
+      <el-tabs :tab-position="tabPosition" style="height: 200px;">
+    <el-tab-pane label="原料介绍">原料介绍</el-tab-pane>
+    <el-tab-pane label="食物相克">食物相克</el-tab-pane>
+    <el-tab-pane label="营养分析">营养分析</el-tab-pane>
+    <el-tab-pane label="适用人群">适用人群</el-tab-pane>
+     <el-tab-pane label="相关内容">相关内容</el-tab-pane>
+  </el-tabs>
+    </el-card>
+    </div>
 </template>
+<script>
+export default {
+    data() {
+      return {
+        tabPosition: 'left'
+      };
+    }
+}
+</script>
--- src/views/scientificMedication.vue
+++ src/views/scientificMedication.vue
@@ -1,13 +1,50 @@
 <template>
-    <div>科学用药</div>
+    <div>
+       <el-card shadow="never" style="margin-bottom: 15px">
+      <div class="search-card">
+        <div class="search-input">
+          <div style="flex-shrink: 0">技术：</div>
+          <el-input size="small" style="margin-left: 10px" placeholder="请输入内容"></el-input>
+        </div>
+        <div>
+          <span>xxx:</span>
+          <el-select size="small" style="margin-left: 10px" placeholder="请选择">
+            <el-option v-for="item in partOptions" :key="item.value" :label="item.label" :value="item.value"> </el-option>
+          </el-select>
+        </div>
+        <div>
+          <span>xxx:</span>
+          <el-select size="small" style="margin-left: 10px" placeholder="请选择">
+            <el-option v-for="item in partOptions" :key="item.value" :label="item.label" :value="item.value"> </el-option>
+          </el-select>
+        </div>
+        <div>
+          <el-button type="search" size="small">查询</el-button>
+        </div>
+      </div>
+    </el-card>
+    </div>
 </template>
 
 <script>
 export default {
-    
+
 }
 </script>
 
 <style scoped>
-
+.search-card {
+  display: flex;
+  align-items: center;
+  font-size: 15px;
+  justify-content: space-between;
+}
+.search-input {
+  display: flex;
+  align-items: center;
+}
+.el-button--search {
+  background: rgb(8, 46, 83);
+  color: #fff;
+}
 </style>
