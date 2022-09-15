3a20e3cbd93b0a854eb9dff784ad5d0c9661eb5e
天往 <2454046178@qq.com>
2022-09-15 18:15:29 +0800

    菜单栏调整

--- src/components/common/CommonAside.vue
+++ src/components/common/CommonAside.vue
@@ -7,7 +7,7 @@
       <img style="width: 20px; height: 20px" :src="item.icon" />
       <span slot="title" style="font-size: 15px; margin-left: 5px">{{ item.label }}</span>
     </el-menu-item>
-    <!-- 有二级的导航栏 -->
+    <!-- 疾病防治 -->
     <el-submenu :index="item.path" v-for="(item, index) in hasChildren" :key="index">
       <template slot="title">
         <!-- <i :class="'el-icon-' + item.icon"></i> -->
@@ -22,12 +22,27 @@
         </el-menu-item>
       </el-menu-item-group>
     </el-submenu>
-    <!-- 最后一个菜单 -->
+    <!-- 倒数第二个菜单 -->
     <el-menu-item :index="item.path" v-for="item in lastChildren" :key="item.path" @click="clickMenu(item)">
       <!-- <i :class="'el-icon-' + item.icon"></i> -->
       <img style="width: 20px; height: 20px" :src="item.icon" />
       <span slot="title" style="font-size: 15px; margin-left: 5px">{{ item.label }}</span>
+      <!-- 最后一个菜单 -->
     </el-menu-item>
+    <el-submenu :index="item.path" v-for="(item, index) in lastHasChildren" :key="index">
+      <template slot="title">
+        <!-- <i :class="'el-icon-' + item.icon"></i> -->
+        <img style="width: 20px; height: 20px" :src="item.icon" />
+        <span style="font-size: 15px; margin-left: 5px">{{ item.label }}</span>
+      </template>
+      <el-menu-item-group>
+        <el-menu-item :index="subItem.path" v-for="(subItem, subIndex) in item.children" :key="subIndex" @click="clickMenu(subItem)">
+          <!-- <i :class="'el-icon-' + subItem.icon"></i> -->
+          <img style="width: 20px; height: 20px" :src="subItem.icon" />
+          <span style="font-size: 15px; margin-left: 5px" slot="title">{{ subItem.label }}</span>
+        </el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
   </el-menu>
 </template>
 <script>
@@ -44,14 +59,17 @@ export default {
     noChildren() {
       // this.asideMenu.filter(item => console.log(item,"导航"))
       // return this.asideMenu.filter((item) => !item.children && item.label != '视频监控' && item.label != '用药指导' && item.label != '种植技术')
-      return this.asideMenu.filter((item) => !item.children && item.label != '视频监控' && item.label != '种植技术')
+      return this.asideMenu.filter((item) => !item.children && item.label != '用药指导')
     },
     hasChildren() {
-      return this.asideMenu.filter((item) => item.children)
+      return this.asideMenu.filter((item) => item.children && item.label === '疾病防治')
+    },
+    lastHasChildren() {
+      return this.asideMenu.filter((item) => item.children && item.label === '柑橘基地(仲恺)')
     },
     lastChildren() {
       // return this.asideMenu.filter((item) => item.label === '视频监控' || item.label === '用药指导' || item.label === '种植技术')
-      return this.asideMenu.filter((item) => item.label === '视频监控' || item.label === '种植技术')
+      return this.asideMenu.filter((item) => item.label === '用药指导')
     },
     isCollapse() {
       return this.$store.state.menu.isCollapse
